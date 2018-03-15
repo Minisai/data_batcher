4690da39f57ee9289138b7beafa9c86386aa1024
Anton Jyha <jyha@1pt.com>
2018-03-15 18:47:16 +0300

    Remove application_cable

--- app/models/event_record.rb
+++ app/models/event_record.rb
@@ -2,6 +2,4 @@ class EventRecord < ApplicationRecord
   validates :value, presence: true, uniqueness: true
 
   belongs_to :events_batch, inverse_of: :event_records, optional: true
-
-  scope :without_batch, -> { where(events_batch_id: nil) }
 end
